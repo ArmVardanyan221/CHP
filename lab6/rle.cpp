82052b333e2ba74cebb4b69ac01eb21e64f385bd
ArmVardanyan221 <152401792+ArmVardanyan221@users.noreply.github.com>
2024-10-12 21:16:09 +0400

    Update rle.cpp

--- lab6/rle.cpp
+++ lab6/rle.cpp
@@ -1,58 +1,55 @@
 #include <iostream>
-#include <cstring>
-#include <unordered_map>
-
+#include <string>
 using namespace std;
 
-void runLengthEncode(const char* input, char* encoded) {
-    int length = strlen(input);
-    int index = 0;
-    unsigned char count = 1; 
+class RunLengthEncoding {
+public:
+    string encode(const string& input) {
+        string encoded = "";
+        int length = input.length();
+        unsigned char count = 1;
 
-    for (int i = 1; i <= length; ++i) {
-        if (i < length && input[i] == input[i - 1]) {
-            count++;
-        } else {
-            encoded[index++] = input[i - 1];   
-            encoded[index++] = count;          
-            count = 1;
+        for (int i = 1; i <= length; ++i) {
+            if (i < length && input[i] == input[i - 1]) {
+                count++;
+            } else {
+                encoded += input[i - 1];
+                encoded += static_cast<char>(count);
+                count = 1;
+            }
         }
+        return encoded;
     }
-    encoded[index] = '\0'; 
-}
 
-void runLengthDecode(const char* encoded, char* decoded) {
-    int index = 0;
-    int decodedIndex = 0;
+    string decode(const string& encoded) {
+        string decoded = "";
+        int index = 0;
 
-    while (encoded[index] != '\0') {
-        char currentChar = encoded[index++];
-        unsigned char count = encoded[index++]; 
+        while (index < encoded.length()) {
+            char currentChar = encoded[index++];
+            unsigned char count = static_cast<unsigned char>(encoded[index++]);
 
-        for (int i = 0; i < count; ++i) {
-            decoded[decodedIndex++] = currentChar;
+            decoded.append(count, currentChar);
         }
+        return decoded;
     }
-    decoded[decodedIndex] = '\0';
-}
+};
 
 int main() {
-    const int MAX_SIZE = 100; 
-    char input[MAX_SIZE];
-    char encoded[MAX_SIZE * 2]; 
-    char decoded[MAX_SIZE];
+    RunLengthEncoding rle;
+    string input;
 
     cout << "Enter a string to encode: ";
     cin >> input;
 
-    runLengthEncode(input, encoded);
+    string encoded = rle.encode(input);
     cout << "Encoded: ";
-    for (int i = 0; encoded[i] != '\0'; i += 2) {
-        cout << encoded[i] << (encoded[i + 1]); 
+    for (size_t i = 0; i < encoded.length(); i += 2) {
+        cout << encoded[i] << (static_cast<unsigned char>(encoded[i + 1]));
     }
     cout << endl;
 
-    runLengthDecode(encoded, decoded);
+    string decoded = rle.decode(encoded);
     cout << "Decoded: " << decoded << endl;
 
     return 0;
